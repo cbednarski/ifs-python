c5cc99195fa8fcd6ccee42b0fa5835d334cf7986
Chris Bednarski <banzaimonkey@gmail.com>
2014-05-22 18:54:17 -0700

    Limit apt-get update to once every 24 hours

--- install-base.sh
+++ install-base.sh
@@ -1,3 +1,11 @@
-# System updates
-apt-get update && apt-get upgrade -y
-apt-get install -y make gcc git vim curl tree
+# Only apt-get update every 24 hours
+echo 'APT::Update::Post-Invoke-Success {"touch /var/lib/apt/periodic/update-success-stamp 2>/dev/null || true";};' > /etc/apt/apt.conf.d/15update-stamp
+now=`date +%s`
+updated=`stat --printf %Y /var/lib/apt/periodic/update-success-stamp || 0`
+expiry=$(($updated+86400))
+if ((expiry < now)); then
+  apt-get update && apt-get upgrade -y
+fi
+
+# Install base dependencies
+apt-get install -y build-essential git vim curl tree
