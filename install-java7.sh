fdb1bf945c2a8968c0a24bbd00473b874cc609b4
Chris Bednarski <banzaimonkey@gmail.com>
2014-08-09 20:59:45 -0700

    Improve java install scripts a bit

--- install-java7.sh
+++ install-java7.sh
@@ -1,6 +1,8 @@
 if ! java -version ; then
+  JAVA_FILE=jdk-7u67-linux-x64.tar.gz
   cd /tmp
-  curl -q -L --cookie 'oraclelicense=accept-securebackup-cookie' http://download.oracle.com/otn-pub/java/jdk/7u67-b01/jdk-7u67-linux-x64.tar.gz -o jdk-7u67-linux-x64.tar.gz
-  tar -xzf jdk-7u67-linux-x64.tar.gz
+  # -s/--silent -L/--location -b/--cookie
+  curl -s -L -b 'oraclelicense=accept-securebackup-cookie' http://download.oracle.com/otn-pub/java/jdk/7u67-b01/$JAVA_FILE -o $JAVA_FILE
+  tar -xzf $JAVA_FILE
   cd /tmp/jdk1.7.0_67
 fi
--- install-java8.sh
+++ install-java8.sh
@@ -1,6 +1,8 @@
 if ! java -version ; then
+  JAVA_FILE=jdk-8u11-linux-x64.tar.gz
   cd /tmp
-  curl -q -L --cookie 'oraclelicense=accept-securebackup-cookie' http://download.oracle.com/otn-pub/java/jdk/8u11-b12/jdk-8u11-linux-x64.tar.gz -o jdk-8u11-linux-x64.tar.gz
-  tar -xzf jdk-8u11-linux-x64.tar.gz
+  # -s/--silent -L/--location -b/--cookie
+  curl -s -L -b 'oraclelicense=accept-securebackup-cookie' http://download.oracle.com/otn-pub/java/jdk/8u11-b12/$JAVA_FILE -o $JAVA_FILE
+  tar -xzf $JAVA_FILE
   cd /tmp/jdk1.8.0_11
 fi
