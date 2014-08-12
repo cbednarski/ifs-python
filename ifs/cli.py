4c37e464769be68d2306d28e8b205c611edc3e4c
Chris Bednarski <banzaimonkey@gmail.com>
2014-08-12 00:12:03 -0700

    Added help text so the CLI does something

--- ifs/cli.py
+++ ifs/cli.py
@@ -3,12 +3,25 @@ import click
 
 @click.group()
 def cli():
+    """
+    Install From Source
+
+    When the version in the package manager has gone stale, get a fresh,
+    production-ready version from a source tarball or precompiled archive.
+
+    Send issues or improvements to https://github.com/cbednarski/ifs
+    """
     pass
 
 
 @cli.command()
 def ls():
     click.echo('list of things')
+
+
+@cli.command()
+def search():
+    click.echo('searching...')
 
 
 @cli.command()
