8d61ab0de2940c16dc44066a718e06b7a2719098
Chris Bednarski <banzaimonkey@gmail.com>
2014-08-23 22:53:15 -0700

    Added some tests

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -1,5 +1,8 @@
 from ifs import lib
 
+class Object(object):
+    pass
+
 def test_list_apps():
     apps = lib.list_apps()
     assert 'nginx' in apps
@@ -23,3 +26,21 @@ def test_app_info():
     info = lib.app_info('nginx')
     assert 'default_version' in info
     assert info['download_url'] == 'http://nginx.org/download/nginx-VERSION.tar.gz'
+
+def test_check_version():
+    app = Object()
+    app.version_cmd = 'venv/bin/py.test --version'
+    assert lib.check_version(app) == '2.6.1'
+
+def test_install_deps():
+    app = Object()
+    app.version_cmd = 'ls'
+
+    app.depends = ['blah']
+    assert lib.install_deps(app) == 'apt-get install -y blah'
+
+    app.depends = []
+    assert lib.install_deps(app) == False
+
+    app.depends = ['blah', 'blee']
+    assert lib.install_deps(app) == 'apt-get install -y blah blee'
