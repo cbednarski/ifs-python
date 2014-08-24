57848fefc2339ecea08724ee79f37be57fb8f3cb
Chris Bednarski <banzaimonkey@gmail.com>
2014-08-23 22:52:13 -0700

    Added Cmd class to simplify checking output and return codes

--- ifs/lib.py
+++ ifs/lib.py
@@ -6,16 +6,36 @@ import importlib
 
 from source import nginx
 
+class Cmd(object):
+    returncode = None
+    cmd = None
+    output = None
+
+    def __init__(self, cmd):
+        cmd = "set -e\n%s" % cmd
+        self.cmd = cmd
+
+    def execute(self):
+        try:
+            self.output = subprocess.check_output(self.cmd, stderr=subprocess.STDOUT, shell=True)
+            self.returncode = 0
+            return True
+        except subprocess.CalledProcessError as e:
+            self.output = e.output
+            self.returncode = e.returncode
+            return False
+
+    @classmethod
+    def run(cls, cmd):
+        cmdo = cls(cmd)
+        cmdo.execute()
+        return cmdo
+
+
 def download(url, filename):
     # Add progress bar via:
     # http://stackoverflow.com/a/22776/317916
     urllib.urlretrieve(url, filename)
-
-def bash(command):
-    try:
-        return subprocess.check_output(command, shell=True)
-    except subprocess.CalledProcessError as e:
-        return False
 
 def list_apps():
     path = os.path.dirname(os.path.realpath(__file__))
@@ -40,26 +60,47 @@ def get_download_url(application, version=None):
         version = app.version
     return app.download_url.replace('VERSION', version)
 
+def match_semver(string):
+    return
+
 def check_version(application):
     # Call app.version_cmd to check which version is currently installed
-    return None
+    cmd = Cmd.run(application.version_cmd)
+    return cmd.output
 
 def app_info(application):
     app = load_app(application)
     info = {
         "default_version": app.version,
-        "current_version": check_version(application) or 'Not Installed',
+        "current_version": check_version(app) or 'Not Installed',
         "dependencies": app.depends,
         "download_url": app.download_url,
     }
     return info
 
+def install_deps(application):
+    if application.depends:
+        return 'apt-get install -y %s' % ' '.join(application.depends)
+    else:
+        return False
+
+def install_app(application, version=None):
+    cmd = application.install_script
+    if not version:
+        version = application.version
+    return cmd.replace('VERSION', version)
+
 def install(application, version=None, force=False):
     app = load_app(application)
     if not version:
         version = app.version
-    installed_version = bash(app.version_cmd)
-    if installed_version:
-        pass
+    if check_version(app) == version:
+        return '%s %s is already installed' % (app.name, version)
     else:
-        pass
+        # Install dependencies
+        deps = Cmd.run(install_deps(app))
+        if deps.returncode > 0:
+            return deps
+
+        install = Cmd.run(install_app(app, version))
+        return install
