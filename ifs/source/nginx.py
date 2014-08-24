84df85bebeb601dbb451924d2a58eb4a4e3774f7
Chris Bednarski <banzaimonkey@gmail.com>
2014-08-23 22:52:31 -0700

    Fixed some issues in nginx install script

--- ifs/source/nginx.py
+++ ifs/source/nginx.py
@@ -1,10 +1,10 @@
 version='1.6.1'
 version_cmd='nginx -v'
-depends=['build-essentials', 'libpcre3-dev', 'zlib1g-dev', 'libssl-dev']
+depends=['build-essential', 'libpcre3-dev', 'zlib1g-dev', 'libssl-dev']
 download_url='http://nginx.org/download/nginx-VERSION.tar.gz'
 install_script="""
-tar -xzf nginx-1.6.0.tar.gz
-cd nginx-1.6.0/
+tar -xzf nginx-VERSION.tar.gz
+cd nginx-VERSION/
 ./configure \
     --with-http_ssl_module \
     --with-http_auth_request_module \
@@ -18,5 +18,5 @@ make
 make install
 
 mkdir -p /etc/nginx/sites-enabled
-chmod -R 0755 /etc/nginx
+chmod -R 0700 /etc/nginx
 """
