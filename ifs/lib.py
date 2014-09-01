625a4713aa0443def4cb987a7dde94fde9d05c6e
Chris Bednarski <banzaimonkey@gmail.com>
2014-08-31 23:52:03 -0700

    Refactored app-related function into class

--- ifs/lib.py
+++ ifs/lib.py
@@ -9,6 +9,46 @@ import urllib
 import click
 
 
+def get_download_filename(url, target):
+    """
+    Given a URL and a path on the filesystem that may be a directory or file,
+    return the filename that we will save our download into.
+
+    E.g: http://blah/file.tar, /tmp -------------> /tmp/file.tar
+    And: http://blah/file.tar, /tmp/archive.tar -> /tmp/archive.tar
+    """
+    if url and os.path.isdir(target):
+        target += '/' + url.split('/')[-1]
+        return target
+    return None
+
+def download(url, target):
+    """
+    Download URL to TARGET on your local filesystem. TARGET should be an
+    absolute path to the location you want to download the file. Useful when
+    curl / wget are not installed, but python is.
+    """
+    # Add progress bar via:
+    # http://stackoverflow.com/a/22776/317916
+    if not url:
+        return None
+    urllib.urlretrieve(url, target)
+    return target
+
+def list_apps():
+    path = os.path.dirname(os.path.realpath(__file__))
+    files = glob.glob(path + '/source/*.py')
+    results = []
+    for f in files:
+        i = os.path.basename(f).split('.')[0]
+        if i not in ['__init__']:
+            results.append(i)
+    return results
+
+def match_semver(string):
+    return
+
+
 class Cmd(object):
     returncode = None
     cmd = None
@@ -36,115 +76,104 @@ class Cmd(object):
         return cmdo
 
 
-def get_download_url(app, version=None):
-    if not version:
-        version = app.version
-    if hasattr(app, 'download_url') and app.download_url:
-        return app.download_url.replace('VERSION', version)
-    else:
-        return None
+class App(object):
+    depends        = None
+    description    = None
+    download_url   = None
+    install_script = None
+    name           = None
+    version        = None
+    version_cmd    = None
 
-def get_download_filename(url, target):
-    if url and os.path.isdir(target):
-        target += '/' + url.split('/')[-1]
-        return target
-    return None
+    def __init__(self, name, module=None):
+        self.name = name
+        if module:
+            for k, v in module.__dict__.iteritems():
+                if k[0] != '_':
+                    setattr(self, k, v)
+        # self.__dict__.update(module.__dict__)
 
-def download(url, target):
-    """
-    Target should be an absolute path to the location you want to download the
-    file.
-    """
-    # Add progress bar via:
-    # http://stackoverflow.com/a/22776/317916
-    if not url:
-        return None
-    urllib.urlretrieve(url, target)
-    return target
+    @classmethod
+    def load(cls, app_name):
+        try:
+            mod = importlib.import_module('ifs.source.%s' % app_name, '..source')
+            app = cls(app_name, mod)
+        except ImportError as e:
+            app = None
+        return app
 
-def list_apps():
-    path = os.path.dirname(os.path.realpath(__file__))
-    files = glob.glob(path + '/source/*.py')
-    results = []
-    for f in files:
-        i = os.path.basename(f).split('.')[0]
-        if i not in ['__init__']:
-            results.append(i)
-    return results
-
-def load_app(app_name):
-    try:
-        mod = importlib.import_module('ifs.source.%s' % app_name, '..source')
-    except ImportError as e:
-        mod = None
-    return mod
-
-def match_semver(string):
-    return
-
-def check_version(app):
-    # Call app.version_cmd to check which version is currently installed
-    cmd = Cmd.run(app.version_cmd)
-    if hasattr(app, 'version_re'):
-        r = re.compile(app.version_re)
-    else:
-        r = re.compile('(\d+\.\d+\.\d+)')
-    matches = r.search(cmd.output)
-    if matches is None:
-        return None
-    else:
-        return matches.group(1)
-
-def app_info(app):
-    info = {
-        "default_version": app.version,
-        "current_version": check_version(app) or 'Not Installed',
-        "dependencies": app.depends,
-        "download_url": app.download_url,
-    }
-    return info
-
-def cmd_install_deps(app):
-    if hasattr(app, 'depends') and app.depends:
-        return 'apt-get install -y %s' % ' '.join(app.depends)
-    else:
-        return None
-
-def cmd_install_app(app, version=None):
-    cmd = app.install_script
-    if not version:
-        version = app.version
-    return cmd.replace('VERSION', version)
-
-def install(app, version=None, force=False):
-    if not version:
-        version = app.version
-
-    # Create temp directory
-    target='/tmp/ifs-%s-%s' % (app.__name__[11:], app.version)
-    if not os.path.exists(target):
-        os.mkdir(target)
-    os.chdir(target)
-
-    # Download source
-    dl_url = get_download_url(app, version)
-    dl_file = get_download_filename(dl_url, target)
-    if dl_file:
-        if os.path.exists(dl_file):
-            click.echo('Already downloaded %s' % dl_file)
+    def get_download_url(self, version=None):
+        if not version:
+            version = self.version
+        if hasattr(self, 'download_url') and self.download_url:
+            return self.download_url.replace('VERSION', version)
         else:
-            click.echo('Downloading %s to %s' % (dl_url, dl_file))
-            if download(dl_url, dl_file):
-                click.echo('Download complete')
+            return None
 
-    # Install dependencies
-    depc = cmd_install_deps(app)
-    if depc:
-        click.echo('Installing dependencies: %s' % depc)
-        deps = Cmd.run(depc)
-        if deps.returncode > 0:
-            return deps
+    def check_version(self):
+        # Call self.version_cmd to check which version is currently installed
+        cmd = Cmd.run(self.version_cmd)
+        if hasattr(self, 'version_re'):
+            r = re.compile(self.version_re)
+        else:
+            r = re.compile('(\d+\.\d+\.\d+)')
+        matches = r.search(cmd.output)
+        if matches is None:
+            return None
+        else:
+            return matches.group(1)
 
-    click.echo('Installing from source')
-    install = Cmd.run(cmd_install_app(app, version))
-    return install
+    def info(self):
+        info = {
+            "description": self.description or self.name,
+            "default_version": self.version or None,
+            "current_version": self.check_version() or 'Not Installed',
+            "dependencies": self.depends or None,
+            "download_url": self.download_url or None,
+        }
+        return info
+
+    def cmd_install_deps(self):
+        if hasattr(self, 'depends') and self.depends:
+            return 'apt-get install -y %s' % ' '.join(self.depends)
+        else:
+            return None
+
+    def cmd_install_app(self, version=None):
+        cmd = self.install_script
+        if not version:
+            version = self.version
+        return cmd.replace('VERSION', version)
+
+    def install(self, version=None, force=False):
+        if not version:
+            version = self.version
+
+        # Create temp directory
+        target='/tmp/ifs-%s-%s' % (self.__name__[11:], self.version)
+        if not os.path.exists(target):
+            os.mkdir(target)
+        os.chdir(target)
+
+        # Download source
+        dl_url = get_download_url(self, version)
+        dl_file = get_download_filename(dl_url, target)
+        if dl_file:
+            if os.path.exists(dl_file):
+                click.echo('Already downloaded %s' % dl_file)
+            else:
+                click.echo('Downloading %s to %s' % (dl_url, dl_file))
+                if download(dl_url, dl_file):
+                    click.echo('Download complete')
+
+        # Install dependencies
+        depc = cmd_install_deps(self)
+        if depc:
+            click.echo('Installing dependencies: %s' % depc)
+            deps = Cmd.run(depc)
+            if deps.returncode > 0:
+                return deps
+
+        click.echo('Installing from source')
+        install = Cmd.run(cmd_install_app(self, version))
+        return install
--- tests/test_lib.py
+++ tests/test_lib.py
@@ -1,59 +1,56 @@
 from ifs import lib
 
-class Object(object):
-    pass
-
 def test_list_apps():
     apps = lib.list_apps()
     assert 'nginx' in apps
     assert '__init__' not in apps
 
 def test_load_app():
-    app = lib.load_app('nginx')
+    app = lib.App.load('nginx')
     assert app.download_url == 'http://nginx.org/download/nginx-VERSION.tar.gz'
 
-    app = lib.load_app('candycorn')
+    app = lib.App.load('candycorn')
     assert app is None
 
 def test_get_download_url():
-    url = lib.get_download_url(lib.load_app('nginx'))
+    url = lib.App.load('nginx').get_download_url()
     assert url == 'http://nginx.org/download/nginx-1.6.1.tar.gz'
 
-    url = lib.get_download_url(lib.load_app('nginx'), '1.6.3')
+    url = lib.App.load('nginx').get_download_url('1.6.3')
     assert url == 'http://nginx.org/download/nginx-1.6.3.tar.gz'
 
 def test_app_info():
-    info = lib.app_info(lib.load_app('nginx'))
+    info = lib.App.load('nginx').info()
     assert 'default_version' in info
     assert info['download_url'] == 'http://nginx.org/download/nginx-VERSION.tar.gz'
 
 def test_check_version():
-    app = Object()
+    app = lib.App('py.test')
     app.version_cmd = 'venv/bin/py.test --version'
-    assert lib.check_version(app) == '2.6.1'
+    assert app.check_version() == '2.6.1'
 
     app.version_re = 'pytest version (\d+\.\d+\.\d+)'
-    assert lib.check_version(app) == '2.6.1'
+    assert app.check_version() == '2.6.1'
 
     app.version_cmd = 'echo "pie"'
-    assert lib.check_version(app) == None
+    assert app.check_version() == None
 
 def test_cmd_install_deps():
-    app = Object()
+    app = lib.App('ls')
     app.version_cmd = 'ls'
-    assert lib.cmd_install_deps(app) == None
+    assert app.cmd_install_deps() == None
 
     app.depends = []
-    assert lib.cmd_install_deps(app) == None
+    assert app.cmd_install_deps() == None
 
     app.depends = ['blah']
-    assert lib.cmd_install_deps(app) == 'apt-get install -y blah'
+    assert app.cmd_install_deps() == 'apt-get install -y blah'
 
     app.depends = ['blah', 'blee']
-    assert lib.cmd_install_deps(app) == 'apt-get install -y blah blee'
+    assert app.cmd_install_deps() == 'apt-get install -y blah blee'
 
 def test_cmd_install_app():
-    app = Object()
+    app = lib.App('application')
     app.version = '1.2.3'
     app.install_script = 'application-VERSION-blah-VERSION'
-    assert lib.cmd_install_app(app) == 'application-1.2.3-blah-1.2.3'
+    assert app.cmd_install_app() == 'application-1.2.3-blah-1.2.3'
