4173e9b1913092690856a780c228a490a65b3c9f
Chris Bednarski <banzaimonkey@gmail.com>
2014-09-05 01:11:19 -0700

    Added jenkins installer

---
+++ ifs/source/jenkins.py
@@ -0,0 +1,9 @@
+version_cmd='java -jar /usr/share/jenkins/jenkins.war --version'
+version_re='(\d\.\d{3})'
+depends=['wget']
+install_script="""
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
+echo "deb http://pkg.jenkins-ci.org/debian binary/" > /etc/apt/sources.list.d/jenkins.list
+apt-get update
+apt-get install -y jenkins
+"""
