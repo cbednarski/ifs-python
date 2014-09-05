db379822d21d5816430651713b938f40e3927cdd
Chris Bednarski <banzaimonkey@gmail.com>
2014-09-05 01:11:37 -0700

    Influxdb removed version and autostart service

--- ifs/source/influxdb.py
+++ ifs/source/influxdb.py
@@ -1,6 +1,6 @@
-version='0.8.1'
 version_cmd='influxdb -v'
 download_url='http://s3.amazonaws.com/influxdb/influxdb_latest_amd64.deb'
 install_script="""
-sudo dpkg -i influxdb_latest_amd64.deb
+dpkg -i influxdb_latest_amd64.deb
+service influxdb start
 """
