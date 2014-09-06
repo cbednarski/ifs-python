8f559132e5d7f449faf470a15ad761bccd5b6c8f
Chris Bednarski <banzaimonkey@gmail.com>
2014-09-05 22:47:00 -0700

    Deleted bash installation scripts that are already converted to installers

--- ifs/source/install-java6.sh
+++
@@ -1,4 +0,0 @@
-cd /tmp
-curl -s -L -b 'oraclelicense=accept-securebackup-cookie' http://download.oracle.com/otn/java/jdk/6u45-b06/jdk-6u45-linux-x64.bin -o jdk-6u45-linux-x64.bin
-chmod +x jdk-6u45-linux-x64.bin
-./jdk-6u45-linux-x64.bin
--- ifs/source/install-node.sh
+++
@@ -1,5 +0,0 @@
-if ! node -v ; then
-  cd /tmp
-  wget -q http://nodejs.org/dist/v0.10.28/node-v0.10.28-linux-x64.tar.gz
-  tar --strip-components 1 -C /usr/local -xzf /tmp/node-v0.10.28-linux-x64.tar.gz
-fi
