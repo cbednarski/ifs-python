b24d66610d46b212f2e7a29aa49d9fd24e63b8aa
Chris Bednarski <banzaimonkey@gmail.com>
2014-09-05 21:44:05 -0700

    Added node.js installer

---
+++ ifs/source/nodejs.py
@@ -0,0 +1,6 @@
+version='0.10.31'
+version_cmd='node -v'
+download_url='http://nodejs.org/dist/vVERSION/node-vVERSION-linux-x64.tar.gz'
+install_script="""
+  tar --strip-components 1 -C /usr/local -xzf node-vVERSION-linux-x64.tar.gz
+"""
