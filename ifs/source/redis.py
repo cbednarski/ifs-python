4a6926ece93dce7ba364baca190e445a9117593c
Chris Bednarski <banzaimonkey@gmail.com>
2014-09-05 21:39:20 -0700

    Added redis installer

---
+++ ifs/source/redis.py
@@ -0,0 +1,10 @@
+version='2.8.14'
+version_cmd='redis-server -v'
+depends=['build-essential']
+download_url='http://download.redis.io/releases/redis-VERSION.tar.gz'
+install_script="""
+tar -xzf redis-VERSION.tar.gz
+cd redis-VERSION/
+make
+make install
+"""
