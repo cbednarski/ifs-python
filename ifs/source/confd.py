847a4dce305532a4d7b4eed9691c6ba9f8b572f6
Chris Bednarski <banzaimonkey@gmail.com>
2014-09-11 00:21:19 -0700

    Added confd installer
    Fix #14

---
+++ ifs/source/confd.py
@@ -0,0 +1,7 @@
+version='0.5.0'
+version_cmd='confd -version'
+download_url='https://github.com/kelseyhightower/confd/releases/download/vVERSION/confd-VERSION-linux-amd64'
+install_script="""
+chmod +x confd-VERSION-linux-amd64
+mv -f confd-VERSION-linux-amd64 /usr/local/bin/confd
+"""
