35ffa4d762f61e9ebc49adc51fcd1f853f506231
Chris Bednarski <banzaimonkey@gmail.com>
2014-09-11 01:09:59 -0700

    Added installer for elastic search
    Fix #10

---
+++ ifs/source/elasticsearch.py
@@ -0,0 +1,10 @@
+version='1.3'
+version_cmd='elasticsearch -version'
+download_url='http://packages.elasticsearch.org/GPG-KEY-elasticsearch'
+install_script="""
+apt-key add GPG-KEY-elasticsearch
+echo "deb http://packages.elasticsearch.org/elasticsearch/VERSION/debian stable main" > /etc/apt/sources.list.d/elasticsearch.list
+apt-get update -qq
+apt-get install -y elasticsearch
+service elasticsearch start
+"""
