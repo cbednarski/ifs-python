1b3333585a6fcfaf7244fdac0b9829ac848a0f5a
Chris Bednarski <banzaimonkey@gmail.com>
2014-09-11 00:28:16 -0700

    Added installer for etcd and etcdctl
    Fix #12

---
+++ ifs/source/etcd.py
@@ -0,0 +1,8 @@
+version='0.4.6'
+version_cmd='etcd -version'
+download_url='https://github.com/coreos/etcd/releases/download/vVERSION/etcd-vVERSION-linux-amd64.tar.gz'
+install_script="""
+tar -xzf etcd-vVERSION-linux-amd64.tar.gz
+mv -f ./etcd-v0.4.6-linux-amd64/etcd /usr/local/bin/etcd
+mv -f ./etcd-v0.4.6-linux-amd64/etcdctl /usr/local/bin/etcdctl
+"""
