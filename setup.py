6a18354dbbb04d44069021e603dd2090e5f97954
Chris Bednarski <banzaimonkey@gmail.com>
2014-09-18 23:44:47 -0700

    Added version 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ifs',
-    version='0.2.0',
+    version='0.3.0',
     author='Chris Bednarksi',
     author_email='banzaimonkey@gmail.com',
     url='https://github.com/cbednarski/ifs',
