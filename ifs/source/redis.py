61889e453bedded410e0c04b0b19cd439720111a
Chris Bednarski <banzaimonkey@gmail.com>
2014-09-27 00:51:53 -0700

    Added a version of the redis installer that uses functions instead of bash (mostly)

--- ifs/source/redis.py
+++ ifs/source/redis.py
@@ -2,9 +2,14 @@ version='2.8.17'
 version_cmd='redis-server -v'
 depends=['build-essential']
 download_url='http://download.redis.io/releases/redis-VERSION.tar.gz'
-install_script="""
-tar -xzf redis-VERSION.tar.gz
-cd redis-VERSION/
-make
-make install
-"""
+def installer():
+    shell('tar -xzf redis-VERSION.tar.gz')
+    cd('redis-VERSION/')
+    shell('make')
+    shell('make install')
+    mkdir('/etc/redis')
+    mkdir('/var/redis')
+    cp('utils/redis_init_script', '/etc/init.d/redis_6379', replace=False)
+    cp('redis.conf', '/etc/redis/6379.conf', replace=False)
+    shell('update-rc.d redis_6379 defaults')
+    shell('service redis_6379 start')
