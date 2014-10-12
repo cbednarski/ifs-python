0551e6b0b91ba56f63f3c4bd408a7ba3231d56e3
Chris Bednarski <banzaimonkey@gmail.com>
2014-10-12 04:00:28 -0700

    Cleaned up install script, added upstart stub

--- ifs/source/haproxy.py
+++ ifs/source/haproxy.py
@@ -1,17 +1,31 @@
-version=''
-version_cmd=''
-depends=['build-essential']
-download_url=''
+version='1.5.5'
+version_cmd='haproxy -v'
+depends=['build-essential','libpcre3-dev','zlib1g-dev','libssl-dev']
+download_url='http://www.haproxy.org/download/1.5/src/haproxy-VERSION.tar.gz'
+
+upstart="""
+start on runlevel [2345]
+stop on runlevel [016]
+
+expect fork
+
+pre-start exec haproxy -t
+pre-stop exec haproxy -s quit
+
+reload signal SIGHUP
+
+exec haproxy
+"""
+
 install_script="""
 echo "This installer is broken :("
 echo "Please help fix it: https://github.com/cbednarski/ifs/issues/9"
-# apt-get install -y libpcre3-dev zlib1g-dev libssl-dev
-# cd /tmp
-# wget -q http://www.haproxy.org/download/1.5/src/haproxy-1.5.3.tar.gz
-# tar -xzf haproxy-1.5.3.tar.gz
-# cd /tmp/haproxy-1.5.3
+# tar -xzf haproxy-VERSION.tar.gz
+# cd haproxy-VERSION
 # make TARGET=generic USE_TFO=1 USE_PCRE=1 USE_STATIC_PCRE=1 USE_PCRE_JIT=1 USE_OPENSSL=1
 # make install
+# if [ ! -d /etc/haproxy ] ; then mkdir -p /etc/haproxy ; fi
+# cp examples/haproxy.cfg /etc/haproxy/haproxy.cfg
 # haproxy -v
 exit 1
 """
