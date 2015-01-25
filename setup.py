5d3526d6c249300d1382a5f530899b50a92a0d87
Chris Bednarski <banzaimonkey@gmail.com>
2015-01-24 17:38:13 -0800

    Update version to 0.6.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ifs',
-    version='0.5.0',
+    version='0.6.0',
     author='Chris Bednarski',
     author_email='banzaimonkey@gmail.com',
     url='https://github.com/cbednarski/ifs',
