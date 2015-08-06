d571551c1b820d56a91cf0f080b06b46b8997710
Chris Bednarski <banzaimonkey@gmail.com>
2015-08-05 17:09:37 -0700

    Fixed some bugs in the go and virtualbox installers

--- ifs/source/golang.py
+++ ifs/source/golang.py
@@ -2,6 +2,7 @@ version = '1.4.2'
 version_cmd = 'go version'
 download_url = 'https://storage.googleapis.com/golang/goVERSION.linux-amd64.tar.gz'
 install_script = """
-tar -c /usr/local -xzf goVERSION.linux-amd64.tar.gz
-cat "export PATH=$PATH:/usr/local/go/bin" > /etc/profile.d/golang
+tar -C /usr/local -xzf goVERSION.linux-amd64.tar.gz
+echo "export PATH=\$PATH:/usr/local/go/bin" > /etc/profile.d/golang.sh
+grep "/usr/local/go/bin" /etc/bash.bashrc >/dev/null || echo "export PATH=\$PATH:/usr/local/go/bin" >> /etc/bash.bashrc
 """
