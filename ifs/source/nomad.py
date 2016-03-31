20139c324c859cec8face17b7ab5992464257789
Chris Bednarski <banzaimonkey@gmail.com>
2016-03-30 19:23:21 -0700

    Added nomad

---
+++ ifs/source/nomad.py
@@ -0,0 +1,8 @@
+version = '0.3.1'
+version_cmd = 'nomad -v'
+depends = ['unzip']
+download_url = 'https://releases.hashicorp.com/nomad/VERSION/nomad_VERSION_linux_amd64.zip'
+install_script = """
+unzip -o nomad_VERSION_linux_amd64.zip
+mv -f nomad /usr/local/bin/nomad
+"""
