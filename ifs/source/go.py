ce5682d88e27790d02309fc55983a995356453f0
Chris Bednarski <banzaimonkey@gmail.com>
2016-04-22 01:38:39 -0700

    Changed 'golang' to 'go' and force-clean the install path to prevent problems installing over an older installation

--- ifs/source/go.py
+++ ifs/source/go.py
@@ -3,6 +3,7 @@ version_cmd = 'go version'
 version_re = 'go version go([\d\.]+)'
 download_url = 'https://storage.googleapis.com/golang/goVERSION.linux-amd64.tar.gz'
 install_script = """
+rm -rf /usr/local/go
 tar -C /usr/local -xzf goVERSION.linux-amd64.tar.gz
 echo "export PATH=\$PATH:/usr/local/go/bin" > /etc/profile.d/golang.sh
 grep "/usr/local/go/bin" /etc/bash.bashrc >/dev/null || echo "export PATH=\$PATH:/usr/local/go/bin" >> /etc/bash.bashrc
