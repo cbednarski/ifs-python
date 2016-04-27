b16d634ee9390864da8de6f8d4e7f9e546c8f772
Chris Bednarski <banzaimonkey@gmail.com>
2016-04-27 16:08:43 -0700

    Update Jenkins source to only update the Jenkins repository

--- ifs/source/jenkins.py
+++ ifs/source/jenkins.py
@@ -4,6 +4,6 @@ depends = ['wget']
 install_script = """
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
 echo "deb http://pkg.jenkins-ci.org/debian binary/" > /etc/apt/sources.list.d/jenkins.list
-apt-get update
+apt-get update -o Dir::Etc::sourcelist="sources.list.d/jenkins.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
 apt-get install -y jenkins
 """
